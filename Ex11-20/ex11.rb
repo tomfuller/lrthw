e68092c8c47c3776f087c87e164e5a5af1a446af
Tom Fuller <tomf95@hotmail.co.uk>
2016-10-15 17:50:35 +0100

    ex11.rb

---
+++ Ex11-20/ex11.rb
@@ -0,0 +1,8 @@
+puts "How old are you?"
+age = gets.chomp
+puts "How tall are you?"
+height = gets.chomp
+puts "How much do you weigh?"
+weight = gets.chomp
+
+puts "So you're #{age} old, #{height} tall and #{weight} heavy."
